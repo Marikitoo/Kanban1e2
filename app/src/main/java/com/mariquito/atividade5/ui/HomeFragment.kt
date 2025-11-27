22abaeee454d147e9df37865703566197d6cc8d4
Arthur Marikito <arthurmariquito21@gmail.com>
2025-11-27 14:41:43 -0300

    passo 72

---
+++ app/src/main/java/com/mariquito/atividade5/ui/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.mariquito.atividade5.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mariquito.atividade5.R
+import com.mariquito.atividade5.databinding.FragmentSplashBinding
+
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+}
