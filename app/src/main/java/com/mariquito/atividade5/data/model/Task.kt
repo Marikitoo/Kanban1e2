ba0705d995cc46f217a69742b67b4d0aadb141d4
Marikitoo <arthurmariquito21@gmail.com>
2026-05-18 18:19:18 -0300

    p104

---
+++ app/src/main/java/com/mariquito/atividade5/data/model/Task.kt
@@ -0,0 +1,11 @@
+package com.mariquito.atividade5.data.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class Task (
+    val id: String,
+    val description: String,
+): Parcelable
